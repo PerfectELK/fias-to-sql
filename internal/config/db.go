0167f1c71816f0342a4d4f89c69166d4d7a6ae67
Perfectelk <perfect-elk@perfect-elk.ru>
2023-04-29 23:30:19 +0300

    Table indexes

--- internal/config/db.go
+++ internal/config/db.go
@@ -1,10 +1,12 @@
 package config
 
 func dbConfig(m map[string]string) {
-	m["DB_DRIVER"] = ""
+	m["DB_DRIVER"] = "MYSQL"
 	m["DB_HOST"] = ""
 	m["DB_PORT"] = ""
 	m["DB_NAME"] = ""
 	m["DB_USER"] = ""
 	m["DB_PASSWORD"] = ""
+	m["DB_OBJECTS_TABLE"] = "fias_objects"
+	m["DB_OBJECTS_HIERARCHY_TABLE"] = "fias_objects_hierarchy"
 }
