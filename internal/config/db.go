c9913ae7bcbf6821287ace85b9b81e35dd745b19
Perfectelk <perfect-elk@perfect-elk.ru>
2023-05-09 18:45:47 +0300

    Default db config and threads and debug to config

--- internal/config/db.go
+++ internal/config/db.go
@@ -2,11 +2,11 @@ package config
 
 func dbConfig(m map[string]string) {
 	m["DB_DRIVER"] = "MYSQL"
-	m["DB_HOST"] = ""
-	m["DB_PORT"] = ""
-	m["DB_NAME"] = ""
-	m["DB_USER"] = ""
-	m["DB_PASSWORD"] = ""
+	m["DB_HOST"] = "127.0.01"
+	m["DB_PORT"] = "3306"
+	m["DB_NAME"] = "fias"
+	m["DB_USER"] = "root"
+	m["DB_PASSWORD"] = "123"
 	m["DB_OBJECTS_TABLE"] = "fias_objects"
 	m["DB_OBJECTS_HIERARCHY_TABLE"] = "fias_objects_hierarchy"
 	m["DB_USE_TEMP_TABLE"] = "false"
