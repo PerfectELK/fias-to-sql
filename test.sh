2f71c7ceb4b81f20eb70899da2e43dc37c8169ce
Perfectelk <perfect-elk@perfect-elk.ru>
2023-05-16 00:05:41 +0300

    Test

--- test.sh
+++ test.sh
@@ -6,4 +6,4 @@ SCRIPT=$(readlink -f "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT")
 cd $SCRIPT_PATH/
 
-export $(grep -v '^#' .env | xargs -d '\r') && go test -v -count=1 ./pkg/db/pgsql
+export $(grep -v '^#' .env | xargs -d '\r') && go test -v -count=1 ./...
